8ebe4da0c89784981525aecb74429f6b0b4ee75f
Justin Strang <Justinstrang100@gmail.com>
2022-09-20 11:34:53 -0400

    possibly checkLeft and checkRight fns

--- game.test.js
+++ game.test.js
@@ -58,7 +58,41 @@ test('gameboard creates 10x10 board of zeros', () => {
     expect(gameboardFactory()).toHaveLength(100);
 })
 
+test('returns true if space is available top', () => {
+    let random = 10;
+    let shipLength = 2;
+    expect(checkTop(board, random, shipLength)).toBeTruthy();
+})
 
+test('returns false if not enough available space top', () => {
+    let random= 10;
+    let shiplength = 5;
+    expect(checkTop(board, random, shiplength)).toBeFalsy();
+})
+
+test('returns true if space is available bottom', () => {
+    let random = 80;
+    let shipLength = 2;
+    expect(checkBottom(board, random, shipLength)).toBeTruthy();
+})
+
+test('returns false if not enough available space bottom', () => {
+    let random= 80;
+    let shiplength = 5;
+    expect(checkBottom(board, random, shiplength)).toBeFalsy();
+})
+
+test('returns false if not enough space left', () => {
+    let random = 10;
+    let shiplength = 3;
+    expect(checkLeft(board, random, shiplength)).toBeFalsy();
+})
+
+test('returns false if not enough space right', () => {
+    let random = 9;
+    let shiplength = 3;
+    expect(checkRight(board, random, shiplength)).toBeFalsy();
+})
 
 /*test('returns true if space is available top', () => {
     let random = 10;
