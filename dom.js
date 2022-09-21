e3dae64686010140219ede881d57aa82272a16f5
Justin Strang <Justinstrang100@gmail.com>
2022-09-21 01:10:23 -0400

    returns array of matching sqaures to ships

--- dom.js
+++ dom.js
@@ -4,31 +4,42 @@ let game = gameboardFactory();
 let playerOne = game.playerOne;
 let playerTwo = game.playerTwo;
 
-const element = document.querySelector('[value="00"');
-element.addEventListener('click', () => {
-    
-})
+window.hitOrMiss = hitOrMiss;
+
+function hitOrMiss(event) {
+    console.log(event.path[0])
+}
 
 function matchSquaresToShips(player) {
+    let shipSquares = [];
     for (let i = 0; i <= 99; i++) {
-        if (i <= 9) {
-            i = Number(0 + "" + i);
-        }
+        /*if (i <= 9) {
+            i = "0" + i;
+        }*/
         const currentElement = document.querySelector(`[value="${i}"`);
         for (let ship in player) {
             let length = player[ship].shipLength.length;
             for (let j = 0; j <= length - 1; j++) {
+                /*if (typeof i === "string") {
+                    let stringValue = player[ship].actualLocation[j].toString()
+                    console.log(stringValue, i)
+                    if (i === stringValue) {
+                        
+                        console.log(player[ship].actualLocation[j])
+                    }
+                }*/
                 if (i === player[ship].actualLocation[j]) {
-                    console.log(true)
+                    shipSquares.push(currentElement)
                 }
             }
         }
     }
+    return shipSquares
 }
 
-window.addEventListener("load", () => {
-    matchSquaresToShips(playerOne)
-}) 
+let squares = matchSquaresToShips(playerOne)
+
+console.log(squares)
 
 
 
