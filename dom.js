8f1568c0140d96839ca719492f61acd1d397f789
Justin Strang <Justinstrang100@gmail.com>
2022-09-21 01:26:45 -0400

    eventlistener for squares with ships

--- dom.js
+++ dom.js
@@ -13,21 +13,10 @@ function hitOrMiss(event) {
 function matchSquaresToShips(player) {
     let shipSquares = [];
     for (let i = 0; i <= 99; i++) {
-        /*if (i <= 9) {
-            i = "0" + i;
-        }*/
         const currentElement = document.querySelector(`[value="${i}"`);
         for (let ship in player) {
             let length = player[ship].shipLength.length;
             for (let j = 0; j <= length - 1; j++) {
-                /*if (typeof i === "string") {
-                    let stringValue = player[ship].actualLocation[j].toString()
-                    console.log(stringValue, i)
-                    if (i === stringValue) {
-                        
-                        console.log(player[ship].actualLocation[j])
-                    }
-                }*/
                 if (i === player[ship].actualLocation[j]) {
                     shipSquares.push(currentElement)
                 }
@@ -39,7 +28,12 @@ function matchSquaresToShips(player) {
 
 let squares = matchSquaresToShips(playerOne)
 
-console.log(squares)
+for (let i = 0; i <= squares.length - 1; i++) {
+    console.log(squares[i])
+    squares[i].addEventListener('click', () => {
+        
+    })
+}
 
 
 
